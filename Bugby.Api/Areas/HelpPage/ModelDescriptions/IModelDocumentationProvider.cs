80a2d59d8dd94848ba60c733675fa3fed7334e68
PDC1\jhawkins <jthawkins96@gmail.com>
2019-11-25 09:24:21 -0500

    Initial commit

---
+++ Bugby.Api/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace Bugby.Api.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
